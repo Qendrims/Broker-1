1a906c66a425be30a9929d56bc908515ebcd1f35
BannKastrati <bannkastrati4@gmail.com>
2022-04-05 12:42:17 +0200

    add pdata and functionalities on posts

--- js/user-posts-list.js
+++ js/user-posts-list.js
@@ -1,8 +1,57 @@
 const postimet=[
 
 {id:1,
-title:"Emri Titullit",
-img:'',
-description:"Pershkrimi i postimit",
-price:'cmimi',
-}];
+title:"Emri Titullit 1",
+img:'broker1.jpg',
+description:"Pershkrimi i postimit 1",
+price:'100$',
+},
+{id:2,
+    title:"Emri Titullit 2",
+    img:'broker2.jpg',
+    description:"Pershkrimi i postimit 2",
+    price:'200$',
+    },
+    {id:3,
+        title:"Emri Titullit 3",
+        img:'broker3.jpg',
+        description:"Pershkrimi i postimit 3",
+        price:'300$',
+        },
+        {id:4,
+            title:"Emri Titullit 4",
+            img:'broker4.jpg',
+            description:"Pershkrimi i postimit 4",
+            price:'400$',
+            }
+
+
+];
+
+
+const main=document.querySelector('#main');
+
+
+
+window.addEventListener('load',function(e){
+        // console.log('alban selmanaj');
+        postimet.forEach(function(p){
+
+            main.innerHTML+=`
+        <div class="user-post">
+            <h1>${p.id}</h1>
+            <img src="../images/${p.img}" alt="image">
+            <h3>${p.description}</h3>
+            <p>${p.price}</p>
+        </div>
+
+        `;
+
+
+
+        })
+
+        
+
+
+})
