4b50a104b71898b8aa3e1d0b5f8137b93d6f8732
arberk6 <arkadriu@gmail.com>
2022-04-07 12:29:12 +0200

    code formatting and comments

--- js/AboutUs.js
+++ js/AboutUs.js
@@ -1,14 +1,37 @@
-var index=0;
-var foto=document.getElementsByClassName("foto");
+var index = 0;
+var foto = document.getElementsByClassName("foto");
 
-function slideshow(){
-    for(i=0;i<foto.length;i++){
-        foto[i].style.display="none";
-    }
-    index++;
-    if(index>foto.length)
-    {index=1;}
-    foto[index-1].style.display="block";
-    setTimeout(slideshow,2000);
+function slideshow() {
+  for (i = 0; i < foto.length; i++) {
+    foto[i].style.display = "none";
+  }
+  index++;
+  if (index > foto.length) {
+    index = 1;
+  }
+  foto[index - 1].style.display = "block";
+  setTimeout(slideshow, 2000);
 }
-window.onload=slideshow();
+
+function slideshowManually() {
+  for (i = 0; i < foto.length; i++) {
+    foto[i].style.display = "none";
+  }
+  if (index > foto.length) {
+    index = 1;
+  }
+  foto[index - 1].style.display = "block";
+}
+
+function nextSlide() {
+  index++;
+  slideshowManually();
+}
+
+function prevSlide() {
+  index--;
+  slideshowManually();
+}
+
+//todo: implement click on button to show next slide
+window.onload = slideshow;
