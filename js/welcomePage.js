ed8f1452648f932b8db218dc21c822cde466124f
adhurimkr <91548962+adhurimkr@users.noreply.github.com>
2022-04-07 21:51:15 +0200

    homepage posts and css changes

--- js/welcomePage.js
+++ js/welcomePage.js
@@ -1,6 +1,18 @@
 let username = localStorage.getItem("broker-username");
 let navListLeft = document.getElementById("list1");
 let navHeader = document.getElementById("homepageHeader");
+let navFixed = navHeader.offsetTop;
+
+function navbarFixed() {
+  if (window.pageYOffset > navFixed) {
+    navHeader.classList.add("fixed")
+  } else {
+    navHeader.classList.remove("fixed");
+  }
+}
+window.onscroll = function(){
+  navbarFixed();
+}
 
 navListLeft.innerHTML +=
   username == null
@@ -48,73 +60,69 @@ let posts = [{
   title: 'Titulli1',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
   category: 'Home',
-  img: '../imgs/grid1.jpg'
+  img: 'grid1.jpg'
 },
 {
   title: 'Titulli2',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
   category: 'Flat',
-  img: '../imgs/grid1.jpg'
+  img: 'grid1.jpg'
 },
 {
   title: 'Titulli1',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
   category: 'Office',
-  img: '../imgs/grid1.jpg'
+  img: 'grid1.jpg'
 },
 {
   title: 'Titulli1',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
   category: 'Home',
-  img: '../imgs/grid1.jpg'
+  img: 'grid1.jpg'
 },
 {
   title: 'Titulli1',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
   category: 'Flat',
-  img: '../imgs/grid1.jpg'
+  img: 'grid1.jpg'
 },
 {
   title: 'Titulli1',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
   category: 'Office',
-  img: '../imgs/grid1.jpg'
+  img: 'grid1.jpg'
 },
 {
   title: 'Titulli1',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
   category: 'Flat',
-  img: '../imgs/grid1.jpg'
+  img: 'grid1.jpg'
 },
 {
   title: 'Titulli1',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
   category: 'Home',
-  img: '../imgs/grid1.jpg'
+  img: 'grid1.jpg'
 },
 {
   title: 'Titulli1',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
   category: 'Flat',
-  img: '../imgs/grid1.jpg'
+  img: 'grid1.jpg'
 },
 ]
-
-let homes = posts.filter(post => post.category == "Home");
-let flats = posts.filter(post => post.category == "Flat");
-let offices = posts.filter(post => post.category == "Office")
 
 let content = document.querySelectorAll('.grid-content');
 content.forEach(cont => {
   let postsFilter = posts.filter(post=> post.category == cont.id).slice(0,3)
-  cont.innerHTML = `<h1 class="category-name">${cont.id}</h1>`;
+  cont.innerHTML = `<h1 class="category-name">${cont.getAttribute('data-text')}</h1>`;
   postsFilter.forEach(post => {
     cont.innerHTML += `
      <div class="homepagegrid content1">
-               <img src=${post.img} alt="Home">
+               <img src='../../imgs/${post.img}' alt="Home">
                <h1>${post.title}</h1>
                <p>${post.description}.</p>
-               <a class="btn btn-primary" href="#">Search homes</a>
+               <a class="btn btn-primary" href="#">Show Details</a>
            </div>
     `
   })
