b5a29949a11c48f8712a5ba944d58c3eb3348232
arberk6 <arkadriu@gmail.com>
2022-04-07 10:50:41 +0200

    code clean up

--- js/login-validate.js
+++ js/login-validate.js
@@ -1,46 +1,48 @@
-let username = document.getElementById('username');
-let password = document.getElementById('password');
+let username = document.getElementById("username");
+let password = document.getElementById("password");
 
-let usernameError = document.getElementById('username_error');
-let passError = document.getElementById('password_error');
+let usernameError = document.getElementById("username_error");
+let passError = document.getElementById("password_error");
 
-
-
-var users=[
-    {
-        username:"Adhurim",
-        email:'adhurim@gmail.com',
-        password:"123123123"
-    },
-    {
-        username:"Lir",
-        email:'lir@gmail.com',
-        password:"liriballata"
-    },
-]; 
+//todo: switch with backend data
+var users = [
+  {
+    username: "adhurim",
+    email: "adhurim@gmail.com",
+    password: "123123123",
+  },
+  {
+    username: "lir",
+    email: "lir@gmail.com",
+    password: "liriballata",
+  },
+];
 
 var verifyUsername = function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-   let isLoggedIn = false;
-    users.forEach(user => {
-        if((username.value == user.username || username.value == user.email) && password.value == user.password){
-            localStorage.setItem('broker-username',user.username);
-           location.href = '../html/views/welcomePage.html';
-   isLoggedIn = true;
-        }
-    });
-
-    if(!isLoggedIn){
-        usernameError.style.display = 'block';
-        username.value = "";
-        passError.style.display='block';
-        password.value = "";
-        username.focus();
+  let hasError = true;
+  users.forEach((user) => {
+    if (
+      (username.value.toLowerCase() == user.username ||
+        username.value.toLowerCase() == user.email) &&
+      password.value == user.password
+    ) {
+      localStorage.setItem("broker-username", user.username);
+      location.href = "../html/views/welcomePage.html";
+      hasError = false;
     }
-}
+  });
 
+  if (hasError) {
+    usernameError.style.display = "block";
+    username.value = "";
+    passError.style.display = "block";
+    password.value = "";
+    username.focus();
+  }
+};
 
-document.getElementById('myForm').addEventListener("submit",function(e){
-    verifyUsername(e);
+document.getElementById("myForm").addEventListener("submit", function (e) {
+  verifyUsername(e);
 });
--- js/welcomePage.js
+++ js/welcomePage.js
@@ -1,27 +1,24 @@
-let username = localStorage.getItem('broker-username');
-let homepageHeader = document.getElementById('homepageHeader');
+let username = localStorage.getItem("broker-username");
+let navListLeft = document.getElementById("list1");
+let navHeader = document.getElementById("homepageHeader");
 
-homepageHeader.innerHTML = username == null ? `
-<ul class="nav-list" id="list1">
-<li id="Home"><a href="#">Home</a></li>
-<li id="About"><a href="#">About</a></li>
-<li id="Contact"><a href="#">Contact</a></li>
-<li id="SignIn"><a href="#" >Sign In</a></li>
+navListLeft.innerHTML +=
+  username == null
+    ? `
+<li id="SignIn"><a href="#" >Sign In</a></li>`
+    : ``;
+
+navHeader.innerHTML +=
+  username == null
+    ? `
+    <ul class="nav-list" id="list2">
+      <li id="Buy"><a href="#">Buy</a></li>
+      <li id="Sell"><a href="#">Sell</a></li>
+      <li id="Rent"><a href="#">Rent</a></li>
+      <li id="Agents"><a href="#">Agents</a></li>
 </ul>
-<img src="../../imgs/logo.jpg" alt="logo">
-<ul class="nav-list" id="list2">
-<li id="Buy"><a href="#">Buy</a></li>
-<li id="Sell"><a href="#">Sell</a></li>
-<li id="Rent"><a href="#">Rent</a></li>
-<li id="Agents"><a href="#">Agents</a></li>
-</ul>
-` :  `
-<ul class="nav-list" id="list1">
-<li id="Home"><a href="#">Home</a></li>
-<li id="About"><a href="#">About</a></li>
-<li id="Contact"><a href="#">Contact</a></li>
-</ul>
-<img src="../../imgs/logo.jpg" alt="logo">
+`
+    : `
 <div class="dropdown">
   <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
     Welcome ${username}
@@ -36,16 +33,13 @@ homepageHeader.innerHTML = username == null ? `
 </div>
 `;
 
+let signIn = document.getElementById("SignIn");
+signIn?.addEventListener("click", () => {
+  location.href = "../login-page.html";
+});
 
-
-let signIn = document.getElementById('SignIn');
-signIn?.addEventListener('click',()=>{
-    
-    location.href= '../login-page.html';
-})
-
-let logout = document.getElementById('LogOut');
-logout?.addEventListener('click',()=>{
-    localStorage.removeItem('broker-username');
-    location.reload();
-})
+let logout = document.getElementById("LogOut");
+logout?.addEventListener("click", () => {
+  localStorage.removeItem("broker-username");
+  location.reload();
+});
