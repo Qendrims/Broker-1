762b812a1dca7d8e3fad6b677bbae01bd9b68f8e
adhurimkr <91548962+adhurimkr@users.noreply.github.com>
2022-04-11 16:44:08 +0200

    create posts based on category with slider

--- js/welcomePage.js
+++ js/welcomePage.js
@@ -3,6 +3,7 @@ let navListLeft = document.getElementById("list1");
 let navHeader = document.getElementById("homepageHeader");
 let navFixed = navHeader.offsetTop;
 
+//setting nav bar to fixed
 function navbarFixed() {
   if (window.pageYOffset > navFixed) {
     navHeader.classList.add("fixed")
@@ -14,6 +15,7 @@ window.onscroll = function(){
   navbarFixed();
 }
 
+//change the inner html of navbar based on whether the user is logged in or not
 navListLeft.innerHTML +=
   username == null
     ? `
@@ -56,6 +58,8 @@ logout?.addEventListener('click',()=>{
     location.reload();
 })
 
+
+//this will change with back end data
 let posts = [{
   title: 'Titulli1',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
@@ -92,6 +96,7 @@ let posts = [{
   category: 'Office',
   img: 'grid1.jpg'
 },
+
 {
   title: 'Titulli1',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
@@ -110,20 +115,104 @@ let posts = [{
   category: 'Flat',
   img: 'grid1.jpg'
 },
+{
+  title: 'Titulli4',
+  description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
+  category: 'Flat',
+  img: 'grid1.jpg'
+},
+{
+  title: 'Titulli5',
+  description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, excepturi voluptatem? Officia repellat praesentium error quibusdam incidunt quasi minus non nihil. Mollitia adipisci nihil illo natus cum sunt blanditiis aliquid.',
+  category: 'Flat',
+  img: 'grid1.jpg'
+},
 ]
 
-let content = document.querySelectorAll('.grid-content');
-content.forEach(cont => {
-  let postsFilter = posts.filter(post=> post.category == cont.id).slice(0,3)
-  cont.innerHTML = `<h1 class="category-name">${cont.getAttribute('data-text')}</h1>`;
+
+//create an array with unique categories 
+let categorySet = new Set(posts.map(p=>p.category));
+let homepagePosts = document.getElementById('homepage-posts');
+
+//for each category we create a div to insert posts based on category
+//whenever a new category is added it creates new html element dynamically
+categorySet.forEach(c=>{
+  homepagePosts.innerHTML += `
+  <h1>${c}s</h1>
+ <div class="slider">
+  <div class="homepage-posts-container slides" data-category="${c}" id="slides"></div>
+  <div  class="prev" id="prev">
+   <i class="fas fa-caret-left"></i>
+   </div>
+   <div  class="next" id="next">
+   <i class="fas fa-caret-right"></i>
+   </div>
+   </div>
+  `
+})
+
+let postsContainer = document.querySelectorAll('.homepage-posts-container')
+
+
+postsContainer.forEach(cont => {
+  //filtering the posts based on data-category and inserting them in their own category
+  let postsFilter = posts.filter(post=> post.category == cont.getAttribute('data-category'));
   postsFilter.forEach(post => {
     cont.innerHTML += `
-     <div class="homepagegrid content1">
-               <img src='../../imgs/${post.img}' alt="Home">
-               <h1>${post.title}</h1>
-               <p>${post.description}.</p>
-               <a class="btn btn-primary" href="#">Show Details</a>
-           </div>
+          <div class="homepage-post-cards content slide">
+                <img src='../../imgs/${post.img}' alt="Home">
+                <h1>${post.title}</h1>
+                <p>${post.description}.</p>
+                <a class="btn btn-primary" href="#">Show Details</a>
+            </div>
     `
   })
 })
+
+
+const prevBtn = document.querySelectorAll('.prev');
+const nextBtn = document.querySelectorAll('.next');
+
+nextBtn.forEach(next=>{
+  next.addEventListener('click',e=>switchSlide(e,'next'));
+})
+
+prevBtn.forEach(prev=>{
+  prev.addEventListener('click',e=>switchSlide(e,'prev'));
+})
+
+
+let indexSlide = 0;
+
+
+function switchSlide(e,arg){
+  // select the parent element of slides
+  let parentEl = e.target.parentElement.parentElement.children[0];
+  // select all slides
+ let allSlides = parentEl.children;
+ //get the width of slide
+ const slideWidth = allSlides[0].offsetWidth;
+ // if there are 3 or less slides set length to 0 so there is no need to slide because the container fits 3 elements
+ //if there are more than 3 slides set length to -3 because container fits 3 elements
+ let slidesLength = allSlides.length <= 3 ? 0 : allSlides.length - 3;
+
+  parentEl.classList.add('transition')    
+  // gap is 5% of parent element width so  divide it by 20
+  const gap = parentEl.offsetWidth / 20;
+
+  if(arg=='next'){  
+parentEl.style.left = `${(parentEl.offsetLeft - slideWidth) - gap}px`
+indexSlide++;
+  } else {
+    parentEl.style.left = `${(parentEl.offsetLeft + slideWidth) + gap}px`
+    indexSlide--;
+  }
+  
+  if(indexSlide == -1){
+   parentEl.style.left =`${parentEl.offsetLeft - (slideWidth * slidesLength) - gap * slidesLength}px`
+   indexSlide = slidesLength;
+   } else if(indexSlide > slidesLength){
+     parentEl.style.left = '0';
+     indexSlide = 0;
+   }
+}
