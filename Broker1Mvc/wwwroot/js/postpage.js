b569eec76f586196175bbc999655c640f724b9a5
Lir07 <93524804+Lir07@users.noreply.github.com>
2022-04-27 00:43:29 +0200

    update mvc

--- Broker1Mvc/wwwroot/js/postpage.js
+++
@@ -1,5 +0,0 @@
-let welcome = document.getElementById('usernameWelcome');
-let username = localStorage.getItem('broker-username');
-if(username != null){
-    welcome.textContent = `Welcome ${username}`
-}
