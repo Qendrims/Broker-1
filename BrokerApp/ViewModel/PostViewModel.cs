e28a25b3f554730a56e3deadb49a58f7b2dd24f7
Qendrims <74713987+Qendrims@users.noreply.github.com>
2022-05-23 12:46:24 +0200

    validimi dhe konvertimi prej Model ne ViewModel

---
+++ BrokerApp/ViewModel/PostViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BrokerApp.ViewModels
+{
+    public class PostViewModel 
+    {
+        public string Title { get; set; }
+
+        public List<IFormFile> Image { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+    }
+}
