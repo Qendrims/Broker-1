1082c9aa7ccfac815a153a2dabbac9444e4f48be
adhurimkr <91548962+adhurimkr@users.noreply.github.com>
2022-05-23 23:39:35 +0200

    added pagination in postpage

--- dotnetwebApp/Broker/Controllers/PostController.cs
+++ dotnetwebApp/Broker/Controllers/PostController.cs
@@ -15,25 +15,56 @@ namespace Broker.Controllers
         {
                 this._db = applicationDb;
         }
-        public IActionResult PostPage()
-        {
-            FilteredPostViewModel posts = new FilteredPostViewModel();
-            posts.FilteredPosts = _db.Posts.ToList();
-            posts.FilteredCategories = _db.Categories.ToList();
-            return View(posts);
-        }
 
-        public IActionResult GetFilteredPosts(string category, string city)
+        //public IActionResult PostPage(int pg =1)
+        //{
+        //    FilteredPostViewModel posts = new FilteredPostViewModel();
+        //    posts.FilteredPosts = _db.Posts.ToList();
+        //    posts.FilteredCategories = _db.Categories.ToList();
+
+        //    const int pageSize = 2;
+        //    if (pg < 1)
+        //        pg = 1;
+
+        //    int postCount = posts.FilteredPosts.Count();
+        //    var pager = new Pagination(postCount, pg, pageSize);
+
+        //    int postSkip = (pg - 1) * pageSize;
+
+        //    posts.FilteredPosts = posts.FilteredPosts.Skip(postSkip).Take(pager.PageSize).ToList();
+        //    this.ViewBag.Pager = pager;
+        //    return View(posts);
+        //   // return View(posts);
+        //}
+
+        public IActionResult PostPage(string category, string city,int pg=1)
         {
 
             FilteredPostViewModel posts = new FilteredPostViewModel();
             posts.FilteredCategories = _db.Categories.ToList();
-            Category cat = _db.Categories.First(c => c.CategoryName == category);
+            Category cat = new Category();
+            if(category != null)
+            {
+             cat = _db.Categories.First(c => c.CategoryName == category);
+            }
             var result = _db.Posts.Where(p => category == null || p.PostCategories.Any(pc => pc.CategoryId == cat.Id))
                 .Where(p => city == null || p.City.ToLower() == city.ToLower()).ToList();
             posts.FilteredPosts = result;
             posts.Category = category;
             posts.City = city;
+
+            const int pageSize = 2;
+            if (pg < 1)
+                pg = 1;
+
+            int postCount = posts.FilteredPosts.Count();
+            var pager = new Pagination(postCount, pg, pageSize);
+
+            int postSkip = (pg - 1) * pageSize;
+
+            posts.FilteredPosts = posts.FilteredPosts.Skip(postSkip).Take(pager.PageSize).ToList();
+            this.ViewBag.Pager = pager;
+
             return View("PostPage", posts);
         }
     }
