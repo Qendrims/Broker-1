378ab772341bb5800854694574fb360ccb2b161b
BleonaCurri <95574521+BleonaCurri@users.noreply.github.com>
2022-05-31 10:26:18 +0200

    Dizajni i faqes per Sponsorizimet

--- dotnetwebApp/Broker/Controllers/HomeController.cs
+++ dotnetwebApp/Broker/Controllers/HomeController.cs
@@ -84,6 +84,11 @@ namespace Broker.Controllers
             return View();
         }
 
+        public IActionResult AdsPayment()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
