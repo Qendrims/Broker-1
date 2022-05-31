22a013fe17c7af0000f9313bda2b97deedec20e1
adhurimkr <adhukra@gmail.com>
2022-05-31 10:51:30 +0200

    update

---
+++ dotnetwebApp/Broker/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using Broker.ApplicationDB;
+using Broker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Broker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        //[HttpGet]
+        //public JsonResult GetSomething()
+        //{
+
+
+        //    List<PostViewModel> posts = new List<PostViewModel>();
+        //    PostViewModel post = new PostViewModel("Home");
+        //    PostViewModel post1 = new PostViewModel("Flat");
+        //    PostViewModel post2 = new PostViewModel("Office");
+        //    PostViewModel post3 = new PostViewModel("Home");
+        //    PostViewModel post4 = new PostViewModel("Flat");
+        //    PostViewModel post5 = new PostViewModel("Home");
+        //    PostViewModel post6 = new PostViewModel("Flat");
+        //    PostViewModel post7 = new PostViewModel("Flat");
+        //    posts.Add(post);
+        //    posts.Add(post1);
+        //    posts.Add(post2);
+        //    posts.Add(post3);
+        //    posts.Add(post4);
+        //    posts.Add(post5);
+        //    posts.Add(post6);
+        //    posts.Add(post7);
+
+
+        //    //HomeViewModel HomeModel = new HomeViewModel();
+        //    //HomeModel.posts = posts;
+        //    //HomeModel.categories = new HashSet<string>(posts.Select(x => x.Category)).ToList();
+        //    //var data = JsonConvert.SerializeObject(HomeModel.categories);
+
+        //    return Json(data);
+        //}
+
+        public IActionResult Index()
+        {
+
+            List<HomeViewModel> homeViewModels = new List<HomeViewModel>();
+
+            var categories = this._db.Categories.ToList();
+
+            foreach(var category in categories)
+            {
+                HomeViewModel model = new HomeViewModel();
+
+                model.category = category;
+                model.posts = this._db.Posts.Where(p => p.PostCategories.Any(x => x.CategoryId == category.CategoryId)).ToList();
+
+               if(model.posts.Count != 0)
+                {
+                homeViewModels.Add(model);
+                }
+            }
+            return View(homeViewModels);
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult PostPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ js/register.js
@@ -0,0 +1,77 @@
+var registeredUser = false;
+
+function Validate(e){
+    e.preventDefault();
+    
+    var name = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var pass = document.getElementById("password").value;
+    var confirmPass = document.getElementById("conf_pass").value;
+    
+    
+    var nameCheck = lastnameCheck = usernameCheck = emailCheck = passCheck = confirmPassCheck = false
+    if(name == "" || name == null){
+        document.getElementById("firstname_err").classList.remove("hidden");
+    }
+    else{
+        nameCheck=true;
+        document.getElementById("firstname_err").classList.add("hidden");
+    }
+    if(lastname == "" || lastname == null){
+        document.getElementById("lastname_error").classList.remove("hidden");
+    }
+    else{
+        lastnameCheck=true;
+        document.getElementById("lastname_error").classList.add("hidden");
+    }
+    if( username == "" || username == null){
+        document.getElementById("username_error").classList.remove("hidden");
+    }
+    else{
+        usernameCheck=true;
+        document.getElementById("username_error").classList.add("hidden");
+    }
+    if(email == "" || email == null){
+        document.getElementById("email_error").classList.remove("hidden");
+    }
+    else{
+        emailCheck=true;
+        document.getElementById("email_error").classList.add("hidden");
+    }
+    if(pass == "" || pass == null){
+        document.getElementById("password_error").classList.remove("hidden");
+    }
+    else{
+        passCheck=true;
+        document.getElementById("password_error").classList.add("hidden");
+    }
+    if(confirmPass== "" || confirmPass!=pass){
+        document.getElementById("confirm_password_error").classList.remove("hidden");
+    }
+    else{
+        confirmPassCheck=true;
+        document.getElementById("confirm_password_error").classList.add("hidden");
+    }
+
+
+    if(nameCheck && lastnameCheck && emailCheck && usernameCheck && passCheck && confirmPassCheck){
+        document.getElementById("sumbit-btn").disabled=false;
+        document.getElementById("sumbit-btn").style.cursor = "pointer";
+        registeredUser=true;
+
+    }
+
+    
+}
+
+
+
+document.querySelectorAll('.together').forEach(element => {
+
+    element.addEventListener("keyup", e => Validate(e));
+})
+
+    
+
